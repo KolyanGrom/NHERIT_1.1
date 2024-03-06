390381d59a2e65e0e46b52fe6e1c9b2d4833dd41
KolyanGrom <kolyangrom@yandex.ru>
2024-03-06 21:45:28 +0700

    Первая стадия написания тестов

---
+++ src/main/java/SimpleTask.java
@@ -0,0 +1,20 @@
+public class SimpleTask extends Task {
+    private String title;
+
+    public SimpleTask(int id, String title) {
+        super(id);
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean matches(String query) {
+        if (title.contains(query)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
